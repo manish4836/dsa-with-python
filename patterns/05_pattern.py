40d7a6ab7c275c6c4f44f7ab742341a7057885b0
manish4836 <yelnemanish@gmail.com>
2026-01-01 23:05:57 +0530

    Day 1: Pattern problems practice

---
+++ patterns/05_pattern.py
@@ -0,0 +1,6 @@
+n = int(input("Enter a Number: "))
+for i in range(1, n):
+    for j in range(i):
+        print(i, end="")
+    print()
+
---
+++ patterns/06_pattern.py
@@ -0,0 +1,10 @@
+# for i in range(0, 5):
+#     for j in range(5, i, -1):
+#         print("*", end="")
+#     print()
+
+for i in range(0, 5):
+    for j in range(5 - i):
+        print("*", end="")
+    print()
+
